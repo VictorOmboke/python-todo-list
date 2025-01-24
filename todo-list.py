e5a62cd97e7ca378698f1fd3988b3e2e3d24265f
Victor Omboke <victoromboke@gmail.com>
2025-01-23 17:22:54 -0800

    Add functionality for removing and displaying tasks along with handling user input error and empty lists.

--- todo-list.py
+++ todo-list.py
@@ -36,9 +36,57 @@ def get_more_tasks (todo_list):
         if prompt == "yes":
             new_task = get_task()
             add_task(todo_list, new_task)
+        elif prompt == "no" and len(todo_list) > 0:
+            display_todo_list(todo_list)
+        elif prompt == "no" and len(todo_list) == 0:
+            print("Have a nice day!")
         else:
             print("Invalid input. Please enter 'Yes' or 'No'.")
-    
+            prompt = "yes"
+
+def remove_task(todo_list):
+    """
+    Remove tasks from the todo list.
+
+    Parameters: 
+    todo_list - updated version of the todo list.
+    index - the index of the task to be removed.
+
+    Returns: Nothing as we are modifying the list.
+    """
+    prompt = "yes"
+    while prompt == "yes":
+        prompt = input("Would you like to remove a task from this list? (Enter 'Yes' or 'No'): ").lower()
+        if prompt == "yes":
+            index = int(input("Please enter the task number from the todo list. (Example: '1', '3', '4'): ")) - 1
+            print(f"Your task '{todo_list[index].get("Task")}' was removed from the todo list.")
+            todo_list.pop(index)
+            if len(todo_list) > 0:  
+                display_todo_list(todo_list)
+            else:
+                prompt = "no"
+                print("Your todo list is empty.")
+                get_more_tasks(todo_list)
+        elif prompt == "no" and len(todo_list) > 0:
+            display_todo_list(todo_list)
+        elif prompt == "no" and len(todo_list) == 0:
+            print("Have a nice day!")
+        else:
+            print("Invalid input, please enter 'Yes' or 'No'.")
+            prompt = "yes"
+
+
+def display_todo_list(todo_list):
+    """
+    Display the current todo list in a user friendly way.
+
+    Parameters: todo_list - the updated todo list.
+    """
+    print("Todo List:")
+    # Loop through todo list using enumerate to create a numbered list.
+    for i, task_info in enumerate(todo_list, start=1):
+        # Display list in an easily comprehensible format.
+        print(f"{i}. {task_info['Task']} - Completed: {task_info['Completed']}")
 
 def execute():
     """
@@ -51,11 +99,7 @@ def execute():
     add_task(todo_list, task)
     # Continue asking the user to enter more tasks if they would like.
     get_more_tasks(todo_list)
-    # Display the current todo list.
-    print("Todo List:")
-    # Loop through todo list using enumerate to create a numbered list.
-    for i, task_info in enumerate(todo_list, start=1):
-        # Display list in an easily comprehensible format.
-        print(f"{i}. {task_info['Task']} - Completed: {task_info['Completed']}")
+    # Ask the user if they would like to remove any tasks.
+    remove_task(todo_list)
 
 execute()
